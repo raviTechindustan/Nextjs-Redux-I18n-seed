5db1244d07f0ac495a56496e6d8bd772b09425fa
raviTechindustan <ravi.techindustan@gmail.com>
2020-07-10 11:18:42 +0530

    create button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-
+import { useRouter } from 'next/router'
 export default function Home() {
+  const router = useRouter();
   const click = () => {
-
+    router.push("/Homepage")
   }
   return (
     <div className="container">
@@ -10,6 +11,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      IF you want to use langauge Translation then click this button.
       <button onClick={click}>click</button>
     </div>
   )
